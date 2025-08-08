0b74e19852ed1fa824fc2154a6937d2f2029b85e
chrish <chris.d.howells@gmail.com>
2025-08-08 13:52:08 +0100

    Initial commit

--- resources/modules/instances/instances.tf
+++ resources/modules/instances/instances.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.13.0"
-    }
-  }
-}
 locals {
   nginx_image = "gcr.io/${var.project_id}/nginx-static-site:v1"
 }
