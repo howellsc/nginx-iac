ae4a2f5c321c78927b45220710440fa7481cba5a
chrish <chris.d.howells@gmail.com>
2025-08-08 13:53:06 +0100

    Initial commit

--- resources/modules/instances/instances.tf
+++ resources/modules/instances/instances.tf
@@ -16,7 +16,7 @@ resource "google_project_iam_member" "gcr_access" {
 }
 
 resource "google_artifact_registry_repository_iam_binding" "artifact_registry_access" {
-  members     = "serviceAccount:${google_service_account.vm_sa.email}"
+  members = ["serviceAccount:${google_service_account.vm_sa.email}"]
   location   = "us"
   repository = "gcr.io/${var.project_id}"
   role       = "roles/artifactregistry.reader"
