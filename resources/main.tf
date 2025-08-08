d04b5860f204b110587d099a337f219c003780ca
chrish <chris.d.howells@gmail.com>
2025-08-08 17:26:35 +0100

    Initial commit

--- resources/main.tf
+++ resources/main.tf
@@ -26,6 +26,7 @@ module "gce_instances" {
   source = "./modules/instances"
 
   project_id      = var.project_id
+  region          = var.region
   zone            = var.zone
   vpc_name        = module.vpc_network.vpc_name
   vpc_subnet_name = module.vpc_network.vpc_subnet_name
