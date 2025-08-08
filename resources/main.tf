54a80d7c790be699714618e6560de487efec9500
howellsc <61081586+howellsc@users.noreply.github.com>
2025-08-08 09:32:59 +0100

    Create main.tf

---
+++ resources/main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project                     = var.project_id
+  region                      = var.region
+  zone                        = var.zone
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "${var.project_id--terraform-state"
+    prefix = "terraform/tfstate"
+  }
+}
+
+resource "google_storage_bucket" "external_state" {
+  name                        = "${var.project_id}-terraform-state"
+  location                    = local.state_bucket_region
+  uniform_bucket_level_access = true
+}
+
+module "gce_instances" {
+  source = "./modules/instances"
+
+  project_id      = var.project_id
+  region          = var.region
+  zone            = var.zone
+  vpc_name        = module.vpc_network.vpc_name
+  vpc_subnet_name = module.vpc_network.vpc_subnet_us1_name
+}
+
+module "vpc_network" {
+  source = "./modules/vpc"
+  region = var.region
+}
