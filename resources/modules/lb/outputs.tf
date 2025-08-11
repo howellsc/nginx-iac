7a16ee9d084854d6d07bce38093cb7f73af62a07
chrish <chris.d.howells@gmail.com>
2025-08-11 13:34:31 +0100

    Use name variable for resources

--- resources/modules/lb/outputs.tf
+++ resources/modules/lb/outputs.tf
@@ -1,3 +1,3 @@
 output "nginx_healthcheck_id" {
-  value = google_compute_health_check.nginx_http_health_check.self_link
+  value = google_compute_region_health_check.nginx_http_health_check.self_link
 }
