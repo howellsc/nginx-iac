2b8a523cb941ae3f5df0def914557791f7e869c1
chrish <chris.d.howells@gmail.com>
2025-08-18 12:54:30 +0100

    Add endpoint tester

--- resources/modules/instances/standalone.tf
+++ resources/modules/instances/standalone.tf
@@ -17,7 +17,8 @@ resource "google_compute_instance" "nginx_standalone" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
+      image = "projects/debian-cloud/global/images/family/debian-11"
+      //image = "debian-cloud/debian-12"
     }
   }
 
