8be11a114b6af8de973b8130c3eac8c78eea23f1
chrish <chris.d.howells@gmail.com>
2025-08-19 15:25:46 +0100

    Add endpoint tester

--- resources/modules/instances/instances.tf
+++ resources/modules/instances/instances.tf
@@ -60,28 +60,14 @@ resource "google_compute_region_instance_group_manager" "nginx_mig" {
     port = 80
   }
 
-  auto_healing_policies {
-    health_check      = google_compute_region_health_check.nginx_mig_http_health_check.self_link
-    initial_delay_sec = 120
-  }
+  # auto_healing_policies {
+  #   health_check      = google_compute_region_health_check.nginx_mig_http_health_check.self_link
+  #   initial_delay_sec = 120
+  # }
 
   lifecycle {
     replace_triggered_by = [google_compute_instance_template.nginx_template]
   }
 
   depends_on = [google_compute_instance_template.nginx_template]
-}
-
-resource "google_compute_region_health_check" "nginx_mig_http_health_check" {
-  name                = "${var.name}-nginx-mig-http-health-check"
-  check_interval_sec  = 30
-  timeout_sec         = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 3
-  region              = var.region
-
-  http_health_check {
-    request_path = "/"
-    port_name    = "http"
-  }
 }
