c19ba7b1a0a8cfa8c2e00a104c47b0fbcda4b483
chrish <chris.d.howells@gmail.com>
2025-08-19 16:16:48 +0100

    Add endpoint tester

--- resources/modules/lb/internallb.tf
+++ resources/modules/lb/internallb.tf
@@ -101,7 +101,7 @@ resource "google_compute_region_backend_service" "nginx_combined_backend" {
   load_balancing_scheme = "INTERNAL_MANAGED"
   region                = var.region
 
-  health_checks = [google_compute_region_health_check.nginx_http_health_check.self_link]
+  //health_checks = [google_compute_region_health_check.nginx_http_health_check.self_link]
 
   backend {
     balancing_mode  = "UTILIZATION"
